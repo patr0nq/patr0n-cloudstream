0fd32387582377c29a88d6d3b983579d9fa2acc7
patr0n <eren1371@gmail.com>
2025-04-26 15:35:46 +0300

    Update Dizifun.kt

--- Dizifun/src/main/kotlin/com/patr0n/Dizifun.kt
+++ Dizifun/src/main/kotlin/com/patr0n/Dizifun.kt
@@ -16,6 +16,10 @@ class Dizifun : MainAPI() {
     override val hasQuickSearch       = false
     override val supportedTypes       = setOf(TvType.TvSeries, TvType.Movie)
 
+    override var sequentialMainPage = true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
+    override var sequentialMainPageDelay       = 250L // ? 0.25 saniye
+    override var sequentialMainPageScrollDelay = 250L // ? 0.25 saniye
+
     override val mainPage = mainPageOf(
         "${mainUrl}/netflix"           to "Netflix Dizileri",
         "${mainUrl}/exxen"            to "Exxen Dizileri",
