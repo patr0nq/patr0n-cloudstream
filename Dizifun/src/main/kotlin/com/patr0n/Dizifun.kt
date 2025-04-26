b53ce9418669756114e402ae893569396d0cfd50
patr0n <eren1371@gmail.com>
2025-04-26 16:01:24 +0300

    Update Dizifun.kt

--- Dizifun/src/main/kotlin/com/patr0n/Dizifun.kt
+++ Dizifun/src/main/kotlin/com/patr0n/Dizifun.kt
@@ -69,7 +69,11 @@ class Dizifun : MainAPI() {
     }
 
     override suspend fun load(url: String): LoadResponse? {
-        val document = app.get(url).document
+        val headers = mapOf(
+            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        )
+
+        val document = app.get(url, headers=headers).document
 
         val title       = document.selectFirst("h1.film")?.text()?.trim() ?: return null
         val poster      = fixUrlNull(document.selectFirst("[property='og:image']")?.attr("content"))
@@ -89,9 +93,9 @@ class Dizifun : MainAPI() {
                 addActors(actors)
             }
         } else {
-            val episodes = document.select("div.episode-box").mapNotNull {
-                val epName    = it.selectFirst("div.episode-name a")?.text()?.trim() ?: return@mapNotNull null
-                val epHref    = fixUrlNull(it.selectFirst("div.episode-name a")?.attr("href")) ?: return@mapNotNull null
+            val episodes = document.select("div.parts-middle").mapNotNull {
+                val epName    = it.selectFirst("a")?.text()?.trim() ?: return@mapNotNull null
+                val epHref    = fixUrlNull(it.selectFirst("a")?.attr("href")) ?: return@mapNotNull null
                 val epEpisode = Regex("(\\d+)\\.Bölüm").find(epName)?.groupValues?.get(1)?.toIntOrNull()
                 val epSeason  = Regex("(\\d+)\\.Sezon").find(epName)?.groupValues?.get(1)?.toIntOrNull() ?: 1
 
@@ -115,9 +119,13 @@ class Dizifun : MainAPI() {
 
     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
         Log.d("DZF", "data » $data")
-        val document = app.get(data).document
+        val headers = mapOf(
+            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        )
 
-        val iframes = document.select("div.player-embed iframe").mapNotNull { it.attr("src") }
+        val document = app.get(data, headers=headers).document
+
+        val iframes = document.select("div.player-embed iframe, div.video-player iframe").mapNotNull { it.attr("src") }
 
         iframes.forEach { iframe ->
             Log.d("DZF", "iframe » $iframe")
