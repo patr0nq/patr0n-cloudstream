a90c888b29a55f67fdb424b6903d05d3a6484477
patr0n <eren1371@gmail.com>
2025-05-08 19:41:12 +0300

    up

---
+++ powerSinema/build.gradle.kts
@@ -0,0 +1,33 @@
+version = 3
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        val apiKey = project.findProperty("tmdbApiKey")?.toString() ?: ""
+        buildConfigField("String", "TMDB_SECRET_API", "\"$apiKey\"")
+    }
+}
+
+cloudstream {
+    authors     = listOf("GitLatte", "patr0nq", "keyiflerolsun")
+    language    = "tr"
+    description = "powerboard`un sinema arşivi"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie")
+    iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powersinema/powersinema.png"
+}
