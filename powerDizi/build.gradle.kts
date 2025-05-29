74e175b61cd724e33a636e22e34a720befe9f959
patr0n <105314623+patr0nq@users.noreply.github.com>
2025-05-30 01:12:13 +0300

    Update build.gradle.kts

--- powerDizi/build.gradle.kts
+++ powerDizi/build.gradle.kts
@@ -1,9 +1,24 @@
 version = 3
 
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        val apiKey = project.findProperty("tmdbApiKey")?.toString() ?: ""
+        buildConfigField("String", "TMDB_SECRET_API", "\"$apiKey\"")
+    }
+}
+
 cloudstream {
-    authors     = listOf("patr0n")
+    authors     = listOf("GitLatte", "patr0nq", "keyiflerolsun")
     language    = "tr"
-    description = "powerDizi"
+    description = "powerboard`un yabancı dizi arşivi"
 
     /**
      * Status int as the following:
